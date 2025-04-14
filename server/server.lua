582bbb259e3079b200e6506a78c8821a263a375e
Forcng <164341281+Forcng@users.noreply.github.com>
2025-04-14 16:59:07 -0400

    Rewrote Server Side (Added Security)

--- server/server.lua
+++ server/server.lua
@@ -1,25 +1,40 @@
 ESX = exports["es_extended"]:getSharedObject()
 
-RegisterNetEvent('forcng:purchaseItem')
-AddEventHandler('forcng:purchaseItem', function(item, cost)
+RegisterNetEvent('forcng:purchaseItem', function(item, cost)
     local xPlayer = ESX.GetPlayerFromId(source)
-    if xPlayer.getMoney() >= cost then
-        xPlayer.removeMoney(cost)
-        xPlayer.addInventoryItem(item, 1)
-        TriggerClientEvent('ox_lib:notify', source, {
-            title = 'Food Stand',
-            description = 'You bought a ' .. item .. ' for $' .. cost,
-            type = 'success',
-            icon = 'fa-solid fa-umbrella-beach',
-            position = 'center-right'
-        })
-    else
-        TriggerClientEvent('ox_lib:notify', source, {
-            title = 'Food Stand',
-            description = 'Not enough money!',
-            type = 'error',
-            icon = 'fa-solid fa-umbrella-beach',
-            position = 'center-right'
-        })
+    local validForSale = false
+    local cashCost = nil
+
+    for _, shop in pairs(Config.TheGuys) do
+        for _, shopItem in pairs(shop.Items) do
+            if shopItem.item == item then
+                cashCost = shopItem.cost
+                validForSale = true
+                break
+            end
+        end
+        if validForSale then break end
+    end
+
+    if validForSale and cashCost == cost then
+        if xPlayer.getMoney() >= cost then
+            xPlayer.removeMoney(cost)
+            xPlayer.addInventoryItem(item, 1)
+            TriggerClientEvent('ox_lib:notify', source, {
+                title = 'Food Stand',
+                description = 'You bought a ' .. item .. ' for $' .. cost,
+                type = 'success',
+                icon = 'fa-solid fa-umbrella-beach',
+                position = 'center-right'
+            })
+        else
+            TriggerClientEvent('ox_lib:notify', source, {
+                title = 'Food Stand',
+                description = 'Not enough money!',
+                type = 'error',
+                icon = 'fa-solid fa-umbrella-beach',
+                position = 'center-right'
+            })
+        end
     end
 end)
